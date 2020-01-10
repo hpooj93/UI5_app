b914ae27ddbd0d9c03bfb2417815d105caaf5b9f
hpooj93 <56989719+hpooj93@users.noreply.github.com>
2020-01-10 14:46:23 +0530

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,9 +3,7 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("UI5_app
-_for_Qunit_testing.UI5_app
-.controller.App", {
+	return Controller.extend("UI5_app_for_Qunit_testing.UI5_app.controller.App", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
