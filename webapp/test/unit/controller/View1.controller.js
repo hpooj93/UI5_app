faf7e6f9ee53809146cc0b38f1f3d58fd27af13a
hpooj93 <56989719+hpooj93@users.noreply.github.com>
2020-01-10 15:06:13 +0530

    Update View1.controller.js

--- webapp/test/unit/controller/View1.controller.js
+++ webapp/test/unit/controller/View1.controller.js
@@ -1,16 +1,40 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"Ui5_application/Ui5_app/controller/View1.controller"
+	"UI5_app_for_Qunit_testing/UI5_app/controller/View1.controller"
 ], function (Controller) {
 	"use strict";
 
 	QUnit.module("View1 Controller");
 
-	QUnit.test("I should test the View1 controller", function (assert) {
+	QUnit.test("I should test the View1 controller", 
+	function (assert) {
 		var oAppController = new Controller();
 		oAppController.onInit();
 		assert.ok(oAppController);
 	});
+	
+	QUnit.module("Price State");
+	function priceStateTestCase(Products) {
+
+  var sState = Products.ProductName;
+
+  Products.assert.strictEqual(sState, Products.ReorderLevel, "Is the first Product name Chai?");
+}
+
+  QUnit.test("The first product name is chai", function (assert) {
+
+  priceStateTestCase.call(this, {
+
+  assert: assert,
+
+  ProductName: "Available",
+
+  ReorderLevel: "Available"
+
+  });
 
 });
+});
+
+
