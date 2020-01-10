cbcef30bda94168b238750db2c38ea6b5fb878ad
hpooj93 <56989719+hpooj93@users.noreply.github.com>
2020-01-10 15:04:44 +0530

    Update opaTests.qunit.js

--- webapp/test/integration/opaTests.qunit.js
+++ webapp/test/integration/opaTests.qunit.js
@@ -5,7 +5,7 @@ sap.ui.getCore().attachInit(function () {
 	"use strict";
 
 	sap.ui.require([
-		"Ui5_application/Ui5_app/test/integration/AllJourneys"
+		"UI5_app_for_Qunit_testing/Sample_UI5App/test/integration/AllJourneys"
 	], function () {
 		QUnit.start();
 	});
