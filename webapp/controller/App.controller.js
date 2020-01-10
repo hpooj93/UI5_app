bf25130b915835c69be2a4b8990bcb5ab1975925
hpooj93 <56989719+hpooj93@users.noreply.github.com>
2020-01-10 14:42:16 +0530

    Create App.controller.js

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,47 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("UI5_app
+_for_Qunit_testing.UI5_app
+.controller.App", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf Sample_UI5App_for_Qunit_testing.Sample_UI5App.view.App
+		 */
+		onInit: function () {
+
+		}
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf Sample_UI5App_for_Qunit_testing.Sample_UI5App.view.App
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf Sample_UI5App_for_Qunit_testing.Sample_UI5App.view.App
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf Sample_UI5App_for_Qunit_testing.Sample_UI5App.view.App
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
